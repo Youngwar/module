3ff879b51f795973a5bfba61b435e9fc3bfee91e
Youngwar <i.bugaev@zavodpriborov.com>
2024-08-11 16:48:39 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -2,27 +2,30 @@ calls = 0
 
 
 def count_calls():
-     global calls
-     calls += 1
-     return calls
+    global calls
+    calls += 1
+    return calls
 
-def string_info():
-    string = input('Введите строку: ')
+
+def string_info(string):
     print({len(string), string.upper(), string.lower()})
     count_calls()
 
-def is_contains ():
-    string = input('Введите искомую строку: ')
-    list_to_search = ((input('Введите список через пробел: ').lower()).split())
 
+def is_contains(string):
+    list_to_search = ((input('Введите список через пробел: ').lower()).split())
 
     if string.lower() in list_to_search and string != '':
         print(True)
-    else: print(False)
+    else:
+        print(False)
 
     count_calls()
-string_info()
-is_contains()
 
+
+print('Введите строку: ')
+string_info(input())
+print('Введите искомую строку: ')
+is_contains(input())
 
 print(calls)
