5621df7251621cae4392109427f71db04db9ba88
Youngwar <i.bugaev@zavodpriborov.com>
2024-09-14 13:11:03 +0300

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -2,28 +2,28 @@ calls = 0
 
 
 def count_calls():
-    global calls
-    calls += 1
-    return calls
+     global calls
+     calls += 1
+     return calls
 
-
-def string_info(string):
-    print({len(string), string.upper(), string.lower()})
+def string_info(string_):
+    
     count_calls()
+    return ({len(string_), string_.upper(), string_.lower()})
 
+def is_contains (str_, list_):
+    
+    k = False
+    for i in range (0, len(list_)):
 
-def is_contains(string, list_to_search = []):
-    if string.lower() in list_to_search and string != '':
-        print(True)
-    else:
-        print(False)
-
+        if str_.lower() in list_[i].lower():
+            k = True
     count_calls()
+    return (k)
 
 
-print('Введите строку: ')
-string_info(input())
-
-is_contains(input('Введите искомую строку: ',), (input('Введите список строк через пробел: ').lower()).split())
-
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
 print(calls)
