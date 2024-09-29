c7bc21a07c1cf99ddb13aadaaf018037e173bf34
Youngwar <i.bugaev@zavodpriborov.com>
2024-09-29 11:17:07 +0300

    Add files via upload

---
+++ module_7_5.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+
+
+os.chdir(r'C:\Users\admin\Desktop\PY')
+directory = os.getcwd()
+for root, dirs, files in os.walk(directory):
+    for dir in dirs:
+        for file in files:
+
+                filepath = os.path.join(directory, file)
+                #print(filepath)
+                filetime = os.path.getmtime(filepath)
+                formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+                #print(formatted_time)
+                filesize = os.path.getsize(filepath)
+                #print(filesize)
+                parent_dir = os.path.dirname(filepath)
+                #print(parent_dir)
+                print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        directory = os.path.join(directory, dir)
