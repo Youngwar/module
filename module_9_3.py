504b9d573b93f1183acea343174def9e4999f216
Youngwar <i.bugaev@zavodpriborov.com>
2024-10-12 14:14:16 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,11 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
+
+second_result = (len(first[x]) == len(second[x]) for x in range(0, len(first)) and range(0, len(second)))
+
+
+print(list(first_result))
+
+print(list(second_result))
